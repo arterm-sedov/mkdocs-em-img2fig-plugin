bef7a8215c4a30ca0da15a4403d619b0c8f50ec5
arterm-sedov <66188814+arterm-sedov@users.noreply.github.com>
2021-12-15 02:07:39 +0300

    Update plugin.py

--- src/plugin.py
+++ src/plugin.py
@@ -8,7 +8,7 @@ class Image2FigurePlugin(BasePlugin):
 
     def on_page_markdown(self, markdown, **kwargs):
       
-        pattern = re.compile(r'!\[(.*?)\]\((.*?)\)', flags=re.IGNORECASE)
+        pattern = re.compile(r'*\!\[(.*?)\]\((.*?)\)', flags=re.IGNORECASE)
         
         markdown = re.sub(pattern,
             r'<figure class="figure-image">\n' + \
