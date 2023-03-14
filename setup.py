e3f791b192c25ed422b199c474c471ee90234e8c
arterm-sedov <66188814+arterm-sedov@users.noreply.github.com>
2023-03-14 16:21:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_file(fname):
 
 setup(
     name='mkdocs-em-img2fig-plugin',
-    version='0.3.2.KB',
+    version='0.3.2.1',
     description='A MkDocs plugin that converts markdown encoded images surrounded by two asterisks or two underscores into <figure> elements.',
 	long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
