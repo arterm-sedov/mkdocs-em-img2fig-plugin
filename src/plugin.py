19fd0a5b082617214ad91d5dc040644cff817c22
arterm-sedov <66188814+arterm-sedov@users.noreply.github.com>
2024-08-26 22:30:58 +0300

    Updated regex in plugin.py

--- src/plugin.py
+++ src/plugin.py
@@ -8,10 +8,10 @@ class Image2FigurePlugin(BasePlugin):
 
     def on_page_markdown(self, markdown, **kwargs):
       
-        pattern = re.compile(r'((_)|\*)!\[(.*?)\]((\()|\[)(.*?(".*?")?)(?(5)\)|\])(?(2)_|\*)', flags=re.IGNORECASE)
+        pattern = re.compile(r'((_)|\*)(!\[(.*?)\]((\()|\[)(.*?(".*?")?)(?(6)\)|\]))(?(2)_|\*)', flags=re.IGNORECASE)
         
         markdown = re.sub(pattern,
-            r'<figure markdown>![\3](\6)<figcaption>\3</figcaption></figure>',                        
+            r'<figure markdown>\3<figcaption>\4</figcaption></figure>',                        
             markdown)            
 
         return markdown
